ab2056ace8ebc84ffc24e67ee4124355a4a1850f
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-21 11:13:24 -0500

    fixed some problems

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
-const handleClick = (event) => {
+const displayFibbs = (event) => {
   event.preventDefault()
-  const input = doceument.querySelector('input')
+  const input = document.querySelector('input')
   const n = parseInt(input.value)
 
 
@@ -18,6 +18,7 @@ const handleClick = (event) => {
     list.appendChild(li)
   }
 }
+
 const fibbonaci = (n) =>{
   let sequence = []
   let temp
@@ -25,18 +26,19 @@ const fibbonaci = (n) =>{
   let b = 1
 
   while (n >= 0) {
-   seqence.push(a)
-   temp = 1
+   sequence.push(a)
+   temp = a
    a = a + b
    b = temp
    n--
   }
+
   return sequence
 }
 
 const main = () => {
-  const button = document.querySelector('button')
-  button.addEventListener('click', handleClick)
+  const input = document.querySelector('input')
+  input.addEventListener('change', displayFibbs)
 }
 
   //document.getElementById('root').textContent = 'Hello, World!'
